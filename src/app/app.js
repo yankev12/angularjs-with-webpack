6591f8dd61921ed5f03332a776bba085173b0e94
yankev <yankevmineo@gmail.com>
2020-01-21 21:56:36 -0300

    criando componentes e diretivas

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import angular from 'angular';
+import routing from './app.config';
+import uirouter from 'angular-ui-router';
+import home from './../features/home';
+import randomNames from './../services/randomNames.service';
+import greeting    from './../directives/greeting.directive';
 
 import '../style/app.css';
 
@@ -18,8 +24,9 @@ class AppCtrl {
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
+angular.module(MODULE_NAME, [uirouter, home, randomNames, greeting])
   .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+  .controller('AppCtrl', AppCtrl)
+  .config(routing);
 
 export default MODULE_NAME;
